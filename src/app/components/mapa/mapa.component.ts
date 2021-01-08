db1c3f80f78a882c46a9428968852d91c517b7f7
Diego Pagini <diegopaginidev@gmail.com>
2021-01-08 10:01:20 -0300

    JSON.stringifly para convertir el array en string y poder guardarlo en el localstorage

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Marcador } from '../../clases/marcador.class';
 
 @Component({
   selector: 'app-mapa',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mapa.component.css'],
 })
 export class MapaComponent implements OnInit {
+  marcadores: Marcador[] = [];
   lat: number = 51.678418;
   lng: number = 7.809007;
 
-  constructor() {}
+  constructor() {
+    const nuevoMarcador = new Marcador(51.678418, 7.809007);
+    this.marcadores.push(nuevoMarcador);
+  }
 
   ngOnInit(): void {}
+
+  agregarMarcador(event) {
+    const coords: { lat: number; lng: number } = event.coords;
+    const nuevoMarcador = new Marcador(coords.lat, coords.lng);
+    this.marcadores.push(nuevoMarcador);
+  }
+
+  guardarStorage() {
+    localStorage.setItem('marcadores', JSON.stringify(this.marcadores));
+  }
 }
